e767c0c4f099a425ef75a9b5a450c941df2f0fbc
Andrea Mansur <apmansur1993@gmail.com>
2017-06-22 02:25:10 +0000

    attempt 3

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,20 @@
+class WikisController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+ resources :wikis
+ 
+
   devise_for :users, controllers: { sessions: 'users/sessions' }
   get 'welcome/about'
   
