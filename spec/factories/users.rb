1de6545163c68c8585cc5cd75d1d1597385775cb
Andrea Mansur <apmansur1993@gmail.com>
2017-06-22 01:20:29 +0000

    wiki model attempt 1

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    name RandomData.random_name
+
+     sequence(:email){|n| "user#{n}@factory.com" }
+     password pw
+     password_confirmation pw
+     #role :member
   end
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   let(:user) { create(:user) }
+   let(:wiki) { create(:wiki) }
+   
+    describe "attributes" do
+     it "has title and body attributes" do
+       expect(wiki).to have_attributes(title: wiki.title, body: wiki.body)
+     end
+   end
 end
