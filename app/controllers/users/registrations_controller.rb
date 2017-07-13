fcb85f33a97c922dec1807928fd0c4905e7a5ab8
Andrea Mansur <apmansur1993@gmail.com>
2017-07-13 02:26:37 +0000

    downgrade

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,42 +3,44 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+     super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+     super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+     super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+     super
+  end
   
   def downgrade
     current_user.standard!
+    flash[:notice] = " #{current_user.email} has been downgraded to a standard account"
+    redirect_to edit_user_registration_path
   end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+     super
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def cancel
+     super
+  end
 
   # protected
 
@@ -53,12 +55,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+     super(resource)
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+     super(resource)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
  resources :wikis
  
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions'}
   get 'welcome/about'
   
    root 'welcome#index'
